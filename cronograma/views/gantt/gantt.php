d7c517814ea05631917a0a6b7e467f26ccef86be
Gustavo Martins <Gustavo Martins@192.168.1.8>
2017-06-22 23:17:42 -0300

    Correções solicitadas na View 2

--- cronograma/views/gantt/gantt.php
+++ cronograma/views/gantt/gantt.php
@@ -1,10 +1,6 @@
 
 <div style="width: 100%; height: 100%" id="scroll">
-    <div class="div_menu" style="margin-left: 5%;">
-        <a class="div_menu_a" href="<?php echo BASE_URL . 'home/' . $_SESSION['nome_perfil'] ?>">Inicio</a>
-        <a class="div_menu_a" href="<?php echo BASE_URL . 'projeto' ?>">/ Projetos</a>
-        <a class="div_menu_a" href="#">/ Gráfico de Gantt</a>
-    </div>
+    
     <?php echo "<label class='tituloGantt'>$projeto</label>";
         echo $gantt; ?>
     <br />
